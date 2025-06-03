52c3a5f8d76399b4c25bd1fa554efd4907f21725
evolunary <w2927864@gmail.com>
2025-06-03 17:14:39 -0500

    feat: add persona engine with state machine, versioned memory tree, and autonomous learning loop

--- src/swarm/worker.ts
+++ src/swarm/worker.ts
@@ -6,7 +6,7 @@
  */
 
 import { parentPort, workerData } from 'worker_threads';
-import { PersonaSubAgent } from '../agent/subagents/persona';
+import { PersonaSubAgent } from '../subAgents/persona';
 
 /**
  * Message structure received from the main thread
